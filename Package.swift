b92334fa8f06194280db64246763ca743ced01b8
genexusbot <builder@genexus.com>
2024-03-21 10:16:26 -0300

    Package version: 1.4.27

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreModule_SD_NotificationsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.4.26")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.4.27")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_NotificationsWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Notifications",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_SD_Notifications-1.4.26.xcframework.zip",
-			checksum: "b94be7bf942cfebacf5874dd2c3639c86e019b9cbcd92d6e09ffe166a9baba96"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_SD_Notifications-1.4.27.xcframework.zip",
+			checksum: "1fd427d454d9a2d40c5a3712b26fdae1f3c6f6e24ba033954eef0a1ac7cee17e"
 		)
 	]
 )
